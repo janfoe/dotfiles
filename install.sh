f0f4051ea585bca1ab8faf30142c48603ce8f98d
Jan Föcking <jan.foecking@d-velop.de>
2024-03-05 15:55:03 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# powerline fonts for zsh agnoster theme
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
+
+# oh-my-zsh & plugins
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
